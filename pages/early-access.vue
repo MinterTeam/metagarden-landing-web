d1fd6c71868d40be4ba6420f3708ecb559a824c5
shrpne <shrpne@gmail.com>
2023-06-05 15:25:09 +0100

    add redirect early-access -> buy-tokens

---
+++ pages/early-access.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    layout: 'metagarden-chain',
+    fetch({redirect, app}) {
+        return redirect(app.i18nGetPreferredPath('/buy-tokens'));
+    },
+};
+</script>
+
+<template>
+    <div class="u-section u-container u-container--small">
+        Loading…
+    </div>
+</template>
